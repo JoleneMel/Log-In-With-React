3da615785651360a34120608e4c8eb211f4951fd
Jolene <74627192+JoleneMel@users.noreply.github.com>
2021-03-07 19:32:54 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import NavBar from './components/navbar.js';
+import LogIn from './components/log-in';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <LogIn />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+export default class NavBar extends React.Component {
+    render() {
+        return (
+            <nav id="menu" className="navbar navbar-expand-sm" >
+                <a className="navbar-brand" href="#" />
+                <ul className="navbar-nav" id="nav">
+                <li class="nav-item active">
+                    {/* per reuest of office hours here are some random links  */}
+                <a className="nav-link" href="https://www.youtube.com/watch?v=CdKZrRomPg4">Watch Out!</a>
+                </li>
+                <li>
+                <a className="nav-link" href="https://www.youtube.com/watch?v=czLYl4fM8yk">Goofy</a>
+                </li>
+                <li>
+                <a className="nav-link" href="https://www.youtube.com/watch?v=oy-3KI2eHi4">#1</a>
+                </li>
+                </ul>
+                
+            
+            </nav>
+    );
+    }
+}
